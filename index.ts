e90063615147808283dac746d37eb9918f0dbbbd
Sushila Kandel <kandel.sushila242@gmail.com>
2025-03-06 21:29:21 +0200

    First commit

---
+++ index.ts
@@ -0,0 +1,11 @@
+
+const myName:string = 'Sushila';
+
+const ten: number = 10;
+const negative: number = -10000;
+const decimal: number = 10.00000001;
+
+const yes: boolean = true;
+
+const nothingHere: null = null;
+const noDefinition: undefined = undefined;
