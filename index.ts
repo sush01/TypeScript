ded1f3fc19aa383678f69b9a9ce7eac4b6ad66dd
Sushila Kandel <kandel.sushila242@gmail.com>
2025-03-07 10:13:43 +0200

    Typescript learning

--- index.ts
+++ index.ts
@@ -9,3 +9,59 @@ const yes: boolean = true;
 
 const nothingHere: null = null;
 const noDefinition: undefined = undefined;
+
+const add = (a:number,b:number): number => {
+  return a+b;
+};
+
+const joinString = (a:string, b:string): string => {
+  return a+b;
+};
+
+const joinStrings = function(a:string, b:string): string {
+  return a+b;
+};
+
+const multiply = (a:number,b:number):number => {
+  return a+b;
+};
+
+//Object 
+const post: {title:string, daysOld:number, published:boolean}= {
+  title: 'Latest Typescript News',
+  daysOld: 10,
+  published: true
+};
+
+//Interface
+interface Post {
+  title:string;
+  daysOld:number; 
+  published:boolean;
+
+}
+const printPost = (postToPrint:  Post) => {
+  return `${postToPrint.title} (${postToPrint.daysOld} days old)`;
+};
+
+//Clasess and properties
+class Car{
+  color: string;
+  year: number;
+
+  constructor(color:string, year:number){
+    this.color = color;
+    this.year = year;
+  }
+
+  drive (){
+    console.log('Vroom');
+  }
+}
+
+const myCar = new Car('red',2000);
+myCar.drive();
+myCar.color = 'green';
+
+//Decorators
+
